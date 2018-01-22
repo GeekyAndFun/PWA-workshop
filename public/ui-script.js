52bcaba46758f7f6aab2ec90a7d4404e2d4c405b
Pafcal <a.pavaloi@gmail.com>
2018-01-22 14:47:02 +0200

    Added messages style & name input

--- public/ui-script.js
+++ public/ui-script.js
@@ -1,10 +1,13 @@
 (function setupUI() {
     const defaultHeightInPixels = '50px';
     const maxHeight = 150;
+    let header = document.getElementsByTagName('header')[0];
     let footer = document.getElementsByTagName('footer')[0];
 
     document.getElementById('headerIconContainer').addEventListener('click', function toggleMenu() {
+        header.classList.toggle('header--with-menu');
         this.classList.toggle('flip');
+        document.body.classList.toggle('menu--visible');
     });
 
     document.getElementsByTagName('textarea')[0].addEventListener('input', function onTextareaInput() {
