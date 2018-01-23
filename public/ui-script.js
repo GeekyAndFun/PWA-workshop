99647f1171684d0fa2b9766bc799af4e12982ac0
Pafcal <a.pavaloi@gmail.com>
2018-01-23 18:01:18 +0200

    Refactoring & added placeholder color. The UI is ready now 🚀

--- public/ui-script.js
+++ public/ui-script.js
@@ -1,22 +1,34 @@
-(function setupUI() {
-    const defaultHeightInPixels = '50px';
-    const maxHeight = 150;
+(function setupUIIife() {
+    const DEFAULT_FOOTER_HEIGHT = '50px';
+    const MAX_FOOTER_HEIGHT = 150;
+    const ONBOARDING_DELAY = 3000;
+
     let header = document.getElementsByTagName('header')[0];
     let footer = document.getElementsByTagName('footer')[0];
+    let ovelay = document.getElementById('overlay');
 
     document.getElementById('headerIconContainer').addEventListener('click', function toggleMenu() {
-        header.classList.toggle('header--with-menu');
-        this.classList.toggle('flip');
         document.body.classList.toggle('menu--visible');
+        if (overlay) {
+            overlay.remove();
+            overlay = null;
+        }
     });
 
     document.getElementsByTagName('textarea')[0].addEventListener('input', function onTextareaInput() {
         if (this.value === '') {
-            footer.style.height = defaultHeightInPixels;
+            footer.style.height = DEFAULT_FOOTER_HEIGHT;
         } else {
-            if (this.scrollHeight < maxHeight) {
+            if (this.scrollHeight < MAX_FOOTER_HEIGHT) {
                 footer.style.height = `${this.scrollHeight}px`;
             }
         }
     });
+
+    if (!window.localStorage.getItem('onboarding')) {
+        setTimeout(() => {
+            overlay.style.display = 'block';
+            window.localStorage.setItem('onboarding', 'true');
+        }, ONBOARDING_DELAY);
+    }
 })();
