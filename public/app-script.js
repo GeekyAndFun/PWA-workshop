cf9d9047169c5e3a31f13a4f02d3a4f288ec4bb7
Pafcal <a.pavaloi@gmail.com>
2018-01-25 11:12:30 +0200

    Added service-worker

---
+++ public/app-script.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../geeky-service-worker.js').then(
+        registration => {
+            console.log(`ServiceWorker registration successful with scope: ${registration.scope}`);
+        },
+        err => {
+            console.error(`Service Worker failed ${err}`);
+        }
+    );
+}
